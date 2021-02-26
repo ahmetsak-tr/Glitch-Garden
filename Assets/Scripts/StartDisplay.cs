28cabf165136d3408ae4619f07f7ad3eae0ffd9d
ahmetsak-tr <ahmeterensak@outlook.com>
2021-02-26 20:20:36 +0300

    updated
    updates

---
+++ Assets/Scripts/StartDisplay.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartDisplay : MonoBehaviour
+{
+    [SerializeField] int stars = 100;
+    Text startText;
+    void Start()
+    {
+        startText = GetComponent<Text>();
+        UpdateDisplay();
+    }
+
+    private void UpdateDisplay()
+    {
+        startText.text = stars.ToString();
+    }
+
+    public void AddStars(int amount)
+    {
+        stars += amount;
+        UpdateDisplay();
+    }
+    
+    public void SpendStars(int amount)
+    {
+        if(amount <= stars)
+        {
+            stars -= amount;
+            UpdateDisplay();
+
+        }
+
+    }
+}
